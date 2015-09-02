4ae4f3fb652c32958093d676b0f9e10d9bd7f5b7
antirek <serge.dmitriev@gmail.com>
2015-09-02 11:20:36 +0700

    try catch some events

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var AsteriskManager = require('asterisk-manager');
 
-var ami = new AsteriskManager('5038','localhost','admin','amp111', true); 
  
+var ami = new AsteriskManager('5038','localhost','admin','superpassword', true);  
 // In case of any connectiviy problems we got you coverd. 
 ami.keepConnected();
  
 // Listen for any/all AMI events. 
-ami.on('managerevent', function (evt) {
-  console.log(evt);
+ami.on('dial', function (evt) {
+  console.log('dial', evt.channel, evt.dialstatus, evt.destination, evt.dialstring);
+});
+
+ami.on('bridge', function (evt) {
+  console.log('bridge', evt.channel1, evt.callerid1, evt.channel2, evt.callerid2);
+});
+
+ami.on('hangup', function (evt) {
+  console.log('hangup', evt.channel, evt.dialstatus, evt.destination);
+});
+
+ami.on('agentcalled', function (evt) {
+  console.log('agentcalled', evt);
 });
