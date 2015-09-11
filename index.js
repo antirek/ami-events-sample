4732f4f2cc85afc8867c6ff4a9cc92663893abae
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-09-11 16:01:08 +0700

    fix

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var AsteriskManager = require('asterisk-manager');
 var uuid = require('node-uuid');
 var Q = require('q');
 
-var ami = new AsteriskManager('5038','.mobilon.ru','','', true);  
+var ami = new AsteriskManager('5038','localhost','admin','superpassword', true);  
 //var ami = new AsteriskManager('5038','p02.mobilon.ru','amiadmin','kug09han', true);  
 // In case of any connectiviy problems we got you coverd. 
 ami.keepConnected();
  
 
-/* 
+
 // Listen for any/all AMI events. 
 ami.on('dial', function (evt) {
   console.log('dial', evt.channel, evt.dialstatus, evt.destination, evt.dialstring);
@@ -26,76 +26,38 @@ ami.on('agentcalled', function (evt) {
   console.log('agentcalled', evt);
 });
 
-*/
-/*
+ami.on('agentconnect', function (evt) {
+  console.log('agentconnect', evt);
+});
+
+ami.on('agentcomplete', function (evt) {
+  console.log('agentcomplete', evt);
+});
+
+ami.on('agentringnoanswer', function (evt) {
+  console.log('agentringnoanswer', evt);
+});
+
+ami.on('join', function (evt) {
+  console.log('join', evt);
+});
+
+ami.on('leave', function (evt) {
+  console.log('leave', evt);
+});
+
+ami.on('queuecallerabandon', function (evt) {
+  console.log('queuecallerabandon', evt);
+});
+
+ami.on('queuecallerjoin', function (evt) {
+  console.log('queuecallerjoin', evt);
+});
+
+ami.on('queuecallerleave', function (evt) {
+  console.log('queuecallerleave', evt);
+});
+
 ami.on('managerevent', function (evt) {
 //	console.log('event', evt);
-})
-*/
-
-
-
-
-var queuestatus = function (amiConnection) {
-	var defer = Q.defer();
-	
-	setTimeout(function() {
-		defer.reject(new Error('timeout for get queue status'))
-	}, 3000);
-
-	var events = {
-		params: [],
-		members: [],
-		entries: []
-	};
-
-	var actionid = uuid.v4();
-
-	var catcher = function (evt) {
-		
-		if (evt.actionid == actionid) {
-			if (evt['event'] == 'QueueParams') events.params.push(evt);
-			if (evt['event'] == 'QueueMember') events.members.push(evt);
-			if (evt['event'] == 'QueueEntry') events.entries.push(evt);
-			if (evt['event'] == 'QueueStatusComplete') { 
-				amiConnection.removeListener('managerevent', catcher); 
-				defer.resolve(events);
-			}
-		};
-	};
-
-	amiConnection.on('managerevent', catcher);
-	amiConnection.action({
-	  'action': 'queuestatus',
-	  'actionid': actionid
-	}, function (err, res) {
-		if (err) defer.reject(err);
-		//console.log('action', err, res);
-	});
-	return defer.promise;
-};
-
-
-
-
-queuestatus(ami)
-	.then(function (data) {
-		console.log(data);
-
-		var membersByQueue = function (queue, events) {
-			var memberEvents = events.filter(function (item) {
-				console.log(item);
-				return (item.queue == queue);
-			})
-			return memberEvents;
-		}
-
-		var q = 'Q93588ef97fdd';
-		console.log('members of', q, ':', membersByQueue(q, data.members));
-
-	})
-	.fail(function (err) {
-		console.log(err);
-	});
-
-
+});
