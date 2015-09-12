290b0b34e20fcca584cef77c92f224d694f2b47d
sergey <serge.dmitriev@gmail.com>
2015-09-12 21:38:23 +0700

    check answer leg1 and leg2

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ami-events-sample
+
+
+[logout]
+exten=>_X.,1,Dial(SIP/${EXTEN},,U(pretech,answer1,${ORIGINATE_ACTIONID}))
+
+[logout2]
+exten=>_X.,1,Dial(SIP/${EXTEN}@gate,,U(pretech,answer2,${ORIGINATE_ACTIONID}))
+
+
+[pretech]
+exten=s,1,NoOp(pretech)
+exten=s,n,Verbose(${CHANNEL(name)})
+exten=s,n,UserEvent(${ARG1},actionid:${ARG2})
+exten=s,n,Return()
+
+
+
--- index.js
+++ index.js
@@ -2,62 +2,28 @@ var AsteriskManager = require('asterisk-manager');
 var uuid = require('node-uuid');
 var Q = require('q');
 
-var ami = new AsteriskManager('5038','localhost','admin','superpassword', true);  
+var ami = new AsteriskManager('5038','localhost','admin','amp111', true);  
 //var ami = new AsteriskManager('5038','p02.mobilon.ru','amiadmin','kug09han', true);  
 // In case of any connectiviy problems we got you coverd. 
 ami.keepConnected();
  
 
-
-// Listen for any/all AMI events. 
-ami.on('dial', function (evt) {
-  console.log('dial', evt.channel, evt.dialstatus, evt.destination, evt.dialstring);
+ami.action({
+  'action':'originate',
+  'channel':'Local/1061@logout',
+  'context':'logout2',
+  'exten':'89135292926',
+  'priority':1,
+  'async': true,
+  'actionid': 12345678,
+  'variable': {
+    'ORIGINATE_ACTIONID': '1234567890'
+  }
+}, function (err, res) {
+  console.log('response', err, res);
 });
 
-ami.on('bridge', function (evt) {
-  console.log('bridge', evt.channel1, evt.callerid1, evt.channel2, evt.callerid2);
-});
 
-ami.on('hangup', function (evt) {
-  console.log('hangup', evt.channel, evt.dialstatus, evt.destination);
-});
-
-ami.on('agentcalled', function (evt) {
-  console.log('agentcalled', evt);
-});
-
-ami.on('agentconnect', function (evt) {
-  console.log('agentconnect', evt);
-});
-
-ami.on('agentcomplete', function (evt) {
-  console.log('agentcomplete', evt);
-});
-
-ami.on('agentringnoanswer', function (evt) {
-  console.log('agentringnoanswer', evt);
-});
-
-ami.on('join', function (evt) {
-  console.log('join', evt);
-});
-
-ami.on('leave', function (evt) {
-  console.log('leave', evt);
-});
-
-ami.on('queuecallerabandon', function (evt) {
-  console.log('queuecallerabandon', evt);
-});
-
-ami.on('queuecallerjoin', function (evt) {
-  console.log('queuecallerjoin', evt);
-});
-
-ami.on('queuecallerleave', function (evt) {
-  console.log('queuecallerleave', evt);
-});
-
-ami.on('managerevent', function (evt) {
-//	console.log('event', evt);
+ami.on('userevent', function (evt) {
+	console.log('event', evt);
 });
